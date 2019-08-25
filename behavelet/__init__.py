78362ac1db3e22879b30e70b7e8dfa38a46f908c
Jake <jgraving@gmail.com>
2019-08-25 11:42:54 +0200

    Initial commit

---
+++ behavelet/__init__.py
@@ -0,0 +1,3 @@
+from morlet import *
+
+__version__ = '0.0.1'
---
+++ setup.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Copyright 2018 Jacob M. Graving <jgraving@gmail.com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import os
+import sys
+import warnings
+from setuptools import setup, find_packages
+
+DESCRIPTION = "a normalized Morlet wavelet for behavioral mapping from Berman et al. (2014)"
+LONG_DESCRIPTION = """\
+a normalized Morlet wavelet for behavioral mapping from Berman et al. (2014)
+"""
+
+DISTNAME = 'behavelet'
+MAINTAINER = 'Jacob Graving <jgraving@gmail.com>'
+MAINTAINER_EMAIL = 'jgraving@gmail.com'
+URL = 'https://github.com/jgraving/behavelet'
+LICENSE = 'Apache 2.0'
+DOWNLOAD_URL = 'https://github.com/jgraving/behavelet.git'
+VERSION = '0.0.1'
+
+
+if __name__ == "__main__":
+
+    setup(name=DISTNAME,
+          author=MAINTAINER,
+          author_email=MAINTAINER_EMAIL,
+          maintainer=MAINTAINER,
+          maintainer_email=MAINTAINER_EMAIL,
+          description=DESCRIPTION,
+          long_description=LONG_DESCRIPTION,
+          license=LICENSE,
+          url=URL,
+          version=VERSION,
+          download_url=DOWNLOAD_URL,
+          install_requires=['numpy',
+                            'cupy',
+                            'multiprocessing'],
+          packages=find_packages(),
+          zip_safe=False,
+          classifiers=['Intended Audience :: Science/Research',
+                       'Programming Language :: Python :: 3']
+          )
